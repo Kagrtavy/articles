41568e5671327c5c5a6c598ba7f4cf035ba963db
Kagrtavy <nesterivan97@gmail.com>
2024-11-28 15:01:55 +0200

    add main

---
+++ app/views/main_template/main.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?= $pageTitle ?? 'Website' ?></title>
+    </head>
+    <body>
+        <header>
+            My Articles Website
+        </header>
+        <main>
+            <?= $content ?>
+        </main>
+        <footer>
+            <div>Made by Ivan Nester</div>
+        </footer>
+    </body>
+</html>
